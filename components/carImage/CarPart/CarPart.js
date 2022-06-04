ce6f3474709cdaae6f0e562ea26e227498afd0d4
Kabanchegg1337 <d-dud@yandex.ru>
2022-06-04 14:36:14 +0300

    Add clicked state to car part

--- components/carImage/CarPart/CarPart.js
+++ components/carImage/CarPart/CarPart.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addPart, removeSelection } from '../../../features/parts/PartsSlice';
 
 export default function CarPart({part}) {
     const dispatch = useDispatch();
+    const status = useSelector(state => state.parts.status);
+    const [clicked, setClicked] = React.useState(false);
     const handleClick = () => {
         if (part.damaged) return dispatch(removeSelection(part))
+        setClicked(true);
         dispatch(addPart(part));
     }
+
+    React.useEffect(() => {
+      if (status === 'pending') return
+      setClicked(false);
+    }, [status])
+
   return (
-    <path d={part.path} fill={part.damaged ? "#f00" : "#000"} onClick={handleClick} />
+    <path d={part.path} fill={part.damaged ? "#f00" : (clicked ? "#0f0" : "#000")} onClick={handleClick} />
   )
 }
