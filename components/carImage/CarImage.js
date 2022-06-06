ebedc185a64825a4b9918e8caefaccf6cee22c72
Kabanchegg1337 <d-dud@yandex.ru>
2022-06-06 19:48:48 +0300

    Add icons to header Fix cross of bounds when zooming image

--- components/carImage/CarImage.js
+++ components/carImage/CarImage.js
@@ -3,89 +3,116 @@ import { useSelector } from "react-redux";
 import styles from "./CarImage.module.scss";
 import CarPart from "./CarPart/CarPart";
 export default function CarImage() {
-    const parts = useSelector(state => state.parts.data)
-    const defaultView = {x0: 0, y0: 0, x1: 237, y1: 331}
-    const [currentView, setCurrentView] = React.useState(
-      {x0: 0, y0: 0, x1: 237, y1: 331}
-    )
-    const [mousePos, setMousePos] = React.useState({
-      x: 0,
-      y: 0,
-    });
-    const [zoom, setZoom] = React.useState(1);
-    const [pointed, setPointed] = React.useState(false);
-    const edges = {
-      x: defaultView.x1 - currentView.x1,
-      y: defaultView.y1 - currentView.y1,
+  const parts = useSelector((state) => state.parts.data);
+  const defaultView = { x0: 0, y0: 0, x1: 237, y1: 331 };
+  const [currentView, setCurrentView] = React.useState(defaultView);
+  const [mousePos, setMousePos] = React.useState({
+    x: 0,
+    y: 0,
+  });
+  const [zoom, setZoom] = React.useState(1);
+  const [pointed, setPointed] = React.useState(false);
+  const edges = {
+    x: defaultView.x1 - currentView.x1,
+    y: defaultView.y1 - currentView.y1,
+  };
+
+  const clamp = (value, min, max) => Math.min(Math.max(value, min), max);
+
+  const handleZoomIn = () => {
+    if (zoom >= 2) return;
+    const newView = {
+      x: currentView.x1 / 1.2,
+      y: currentView.y1 / 1.2,
     };
+    const newEdges = {
+      x: defaultView.x1 - newView.x,
+      y: defaultView.y1 - newView.y,
+    };
+    const newViewPos = {
+      x: clamp(currentView.x0 * 1.2, 0, newEdges.x),
+      y: clamp(currentView.y0 * 1.2, 0, newEdges.y),
+    };
+    setCurrentView({
+      x0: newViewPos.x,
+      y0: newViewPos.y,
+      x1: newView.x,
+      y1: newView.y,
+    });
+    setZoom((curr) => curr + 0.2);
+  };
+  const handleZoomOut = () => {
+    if (zoom <= 1) return;
+    const newView = {
+      x: currentView.x1 * 1.2,
+      y: currentView.y1 * 1.2,
+    };
+    const newEdges = {
+      x: defaultView.x1 - newView.x,
+      y: defaultView.y1 - newView.y,
+    };
+    const newViewPos = {
+      x: clamp(currentView.x0 / 1.2, 0, newEdges.x),
+      y: clamp(currentView.y0 / 1.2, 0, newEdges.y),
+    };
+    setCurrentView({
+      x0: newViewPos.x,
+      y0: newViewPos.y,
+      x1: newView.x,
+      y1: newView.y,
+    });
+    setZoom((curr) => curr - 0.2);
+  };
 
-    const handleZoomIn = () => {
-      if (zoom >= 2) return
-      setCurrentView(curr => {
-        return {...curr, 
-          x0: curr.x0 * 1.2,
-          y0: curr.y0 * 1.2,
-          x1: curr.x1 / 1.2,
-          y1: curr.y1 / 1.2
-        }
-      })
-      setZoom(curr => curr + 0.2);
-    }
-    const handleZoomOut = () => {
-      if (zoom <= 1) return
-      setCurrentView(curr => {
-        return {...curr, 
-          x1: curr.x1 * 1.2,
-          y1: curr.y1 * 1.2,
-          x0: curr.x0 / 1.2,
-          y0: curr.y0 / 1.2,
-        }
-      })
-      setZoom(curr => curr - 0.2)
-    }
-
-    const handleTouchStart = (x, y) => {
-      setPointed(true);
-      setMousePos({x: x, y: y})
-    }
-    const handleMove = (x,y) => {
-      if (!pointed) return;
-      const diffX = mousePos.x - x
-      const diffY = mousePos.y - y
-      const newPos = {
-        x: currentView.x0 + diffX,
-        y: currentView.y0 + diffY
-      }
-      setCurrentView(curr => {
-        return {...curr, 
-          x0: newPos.x < 0 ? 0 : (newPos.x > edges.x ? edges.x : newPos.x),
-          y0: newPos.y < 0 ? 0 : (newPos.y > edges.y ? edges.y : newPos.y)
-        }
-      })
-      setMousePos({x: x, y: y})
-    }
-    const handleTouchEnd = () => {
-      setPointed(false);
-    }
+  const handleTouchStart = (x, y) => {
+    setPointed(true);
+    setMousePos({ x: x, y: y });
+  };
+  const handleMove = (x, y) => {
+    if (!pointed) return;
+    const diffX = mousePos.x - x;
+    const diffY = mousePos.y - y;
+    const newPos = {
+      x: currentView.x0 + diffX,
+      y: currentView.y0 + diffY,
+    };
+    setCurrentView((curr) => {
+      return {
+        ...curr,
+        x0: clamp(newPos.x, 0, edges.x),
+        y0: clamp(newPos.y, 0, edges.y),
+      };
+    });
+    setMousePos({ x: x, y: y });
+  };
+  const handleTouchEnd = () => {
+    setPointed(false);
+  };
   return (
     <div className={styles.image_container}>
-        <svg 
-          xmlns="http://www.w3.org/2000/svg" 
-          viewBox={Object.values(currentView).join(' ')} 
-          onTouchMove={e => handleMove(e.touches[0].clientX, e.touches[0].clientY)} 
-          onTouchStart={e => handleTouchStart(e.touches[0].clientX, e.touches[0].clientY)}
-          onPointerMove={e => handleMove(e.clientX, e.clientY)}
-          onPointerDown={(e) => handleTouchStart(e.clientX, e.clientY)}
-          onTouchEnd={handleTouchEnd}
-          onPointerUp={handleTouchEnd}
-          onMouseLeave={handleTouchEnd}
-        >
-            {parts.map(part => <CarPart key={part.id} part={part} />)}
-        </svg>
-        <div className={styles.zoom_buttons}>
-          <button onClick={handleZoomIn}>+</button>
-          <button onClick={handleZoomOut}>-</button>
-        </div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox={Object.values(currentView).join(" ")}
+        onTouchMove={(e) =>
+          handleMove(e.touches[0].clientX, e.touches[0].clientY)
+        }
+        onTouchStart={(e) =>
+          handleTouchStart(e.touches[0].clientX, e.touches[0].clientY)
+        }
+        onPointerMove={(e) => handleMove(e.clientX, e.clientY)}
+        onPointerDown={(e) => handleTouchStart(e.clientX, e.clientY)}
+        onTouchEnd={handleTouchEnd}
+        onPointerUp={handleTouchEnd}
+        onMouseLeave={handleTouchEnd}
+      >
+        {parts.map((part) => (
+          <CarPart key={part.id} part={part} />
+        ))}
+      </svg>
+      <div className={styles.zoom_buttons}>
+        <button onClick={handleZoomIn}>+</button>
+        <button onClick={handleZoomOut}>-</button>
+      </div>
     </div>
   );
 }
--- components/header/Header.js
+++ components/header/Header.js
@@ -1,15 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
 import styles from './Header.module.scss'
+import back from '../../public/back.svg'
+import map from '../../public/map.svg'
 
 export default function Header() {
   return (
     <header className={styles.header}>
-        <div className={styles.back}></div>
+        <div className={styles.back}>
+          <Image src={back} fill="true" width="100%" height="100%" />
+        </div>
         <div className={styles.info}>
             <h1 className={styles.title}>Повреждения</h1>
             <h2 className={styles.subtitle}>Участник "А" VW Polo</h2>
         </div>
-        <div className={styles.icon}></div>
+        <div className={styles.map}>
+          <Image src={map} fill="true" width="100%" height="100%" />
+        </div>
     </header>
   )
 }
